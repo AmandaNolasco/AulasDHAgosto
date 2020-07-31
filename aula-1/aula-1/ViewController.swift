d50057f656dfc85f250ffd10c359f01342044371
Amanda Nolasco <nolasco.amanda@outlook.com>
2020-07-31 17:26:19 -0300

    inseri uma label

---
+++ aula-1/aula-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aula-1
+//
+//  Created by Adriel Alves on 31/07/20.
+//  Copyright © 2020 Digital House. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
